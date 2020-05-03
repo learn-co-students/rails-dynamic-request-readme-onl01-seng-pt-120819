63ce889e2f158298a8ae180e7976e6c3ad44e79f
github username <github email address>
2020-05-02 21:59:22 -0700

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+
+  # get 'posts/:id', to: 'posts#show'
+
+  #Instead of the long, drawn out get route we placed in our routes.rb file, we can use Ruby's RESTful defaults and the resources method. 
+  #Problem is, we only have one of the seven RESTful routes. 
+  #Thankfully, we can pass in an only option, selecting which of the seven RESTful routes we care about. 
+  #In this case, we only care about the show action
+  
+  resources :posts, only: :show
+  
 end
